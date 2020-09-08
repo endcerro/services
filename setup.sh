53dda6cda8291fc0d1bbf0cb5612bdfa85eb908c
Enzo Dal Cerro <edal--ce@student.42.fr>
2020-09-08 20:25:08 +0200

    now i kinda get it

--- setup.sh
+++ setup.sh
@@ -16,12 +16,19 @@
 # minikube start --vm-driver=virtualbox
 
 #Building containers
-docker build -t nginx ./Sources/Images/nginx/.
-docker build -t ftps ./Sources/Images/ftps/.
+
+#DEPLOY NGINX
+docker build -t mynginx ./Sources/Images/nginx/.
+kubectl apply -f ./Sources/nginx/nginx.yaml
+kubectl get pods -l run=mynginx -o wide
+
+#kubectl expose deployment/mynginx
+
+#docker build -t ftps ./Sources/Images/ftps/.
 
 #Starting containers
-docker run -p 80:80 -p 443:443 --rm --name nginx -d nginx
-docker run -p 21:21 --rm --name ftps -d ftps
+#docker run -p 80:80 -p 443:443 --rm --name nginx -d nginx
+#docker run -p 21:21 --rm --name ftps -d ftps
 
 
 #docker run -p 127.0.0.1:80:80 -p 127.0.0.1:8080:8080 -p 127.0.0.1:8081:8081 -it latest

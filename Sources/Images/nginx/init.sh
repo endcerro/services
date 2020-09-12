b9bae885ae8cc774e99b414e993b624d3e6a9734
Enzo Dal Cerro <gmx1pt@gmail.com>
2020-09-12 19:17:49 +0200

    makes sense now'

--- Sources/Images/nginx/init.sh
+++ Sources/Images/nginx/init.sh
@@ -6,10 +6,7 @@ echo "enzo:enzo" | chpasswd
 /usr/sbin/sshd
 
 nginx -g "daemon off;"
-
-
+#bash
 
 #SSL KEYGEN
 #openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout nginx.key -out nginx.crt
-#ENTRYPOINT
-#bash

ec2efbdd0df4b9929cfe3ef58a9a1e36d469d960
Enzo <enzodalcerro@gmail.com>
2020-09-14 17:18:53 +0200

    vsftp service

--- setup.sh
+++ setup.sh
@@ -20,6 +20,7 @@ if [ $# -eq 1 ]
     kubectl delete -f ./Sources/Images/mysql/service.yaml
     kubectl delete -f ./Sources/Images/phpmyadmin/service.yaml
     kubectl delete -f ./Sources/Images/wordpress/service.yaml
+    kubectl delete -f ./Sources/Images/myftps/service.yaml
 fi
 
 #BUILD IMAGES
@@ -28,9 +29,11 @@ docker build -t mynginx ./Sources/Images/nginx/.
 docker build -t mysql ./Sources/Images/mysql/.
 docker build -t myphpmyadmin ./Sources/Images/phpmyadmin/.
 docker build -t mywordpress ./Sources/Images/wordpress/.
+docker build -t myftps ./Sources/Images/ftps/.
 eval $(minikube docker-env)
 #DEPLOY SERVICE
 kubectl apply -f ./Sources/Images/nginx/service.yaml
 kubectl apply -f ./Sources/Images/mysql/service.yaml
 kubectl apply -f ./Sources/Images/phpmyadmin/service.yaml
 kubectl apply -f ./Sources/Images/wordpress/service.yaml
+kubectl apply -f ./Sources/Images/ftps/service.yaml

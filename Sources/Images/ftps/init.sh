f63488df69a4dc5cffc3164af9f7d0ffe9021485
Enzo <enzodalcerro@gmail.com>
2020-09-15 16:27:03 +0200

    PSV MODE more like BAD MODE

--- Sources/Images/ftps/init.sh
+++ Sources/Images/ftps/init.sh
@@ -31,5 +31,6 @@ mkdir /var/run/vsftpd/empty
 #openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/certs/vsftpd.pem -out /etc/certs/vsftpd.pem
 
 #NEED TO CREATE SOME FOLDER AND WE ARE GOOD TO GO
-vsftpd
+#vsftpd
+vsftpd -opasv_min_port=21000 -opasv_max_port=21000
 #bash

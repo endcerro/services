8aee572d6caaa8e130605d1ee17250d62b9f7ba5
Enzo Dal Cerro <gmx1pt@gmail.com>
2020-09-19 18:51:20 +0200

    missing telegraf

---
+++ Sources/Images/telegraf/init.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+
+#adduser -D enzo
+#echo "enzo:enzo" | chpasswd
+
+# chmod -R 777 /
+export TELEGRAF_CONFIG_PATH=/etc/telegraf.conf
+bash
+
+#telegraf
+#grafana-server -homepath usr/share/grafana

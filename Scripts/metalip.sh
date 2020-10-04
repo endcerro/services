6ffddfe4130f32864e4b6b5a28ab24baad981081
Enzo Dal Cerro <edal--ce@student.42.fr>
2020-10-04 18:11:35 +0200

    improving startup

--- Scripts/metalip.sh
+++ Scripts/metalip.sh
@@ -1,9 +1,8 @@
-baseip=$(minikube ip 2>&1)
-endip=$(echo $baseip | rev | cut -c4- | rev 2>&1)
-endip=$(echo "$endip"'245' 2>&1)
-
 cp ./Sources/Services/metallbT.yaml ./Sources/Services/metallb.yaml
 
+baseip=$(minikube ip 2>&1)
+endip=$(echo $baseip | sed 's/\.[0-9]*$/.0/' | rev | cut -c2- | rev 2>&1)
+endip=$(echo "$endip"'245' 2>&1)
 echo "$baseip""-""$endip" >> ./Sources/Services/metallb.yaml
 cp ./Sources/Images/telegraf/telegrafT.conf ./Sources/Images/telegraf/telegraf.conf
 echo "	url = \"https://""$baseip"":10250\"" >> ./Sources/Images/telegraf/telegraf.conf

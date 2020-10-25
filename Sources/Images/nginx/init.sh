6c5b937db9354672536ad84efe5595933150b555
Y Tho <fuck.you@ntm.com>
2020-10-25 17:34:31 +0100

    done 4 real

--- Sources/Images/nginx/init.sh
+++ Sources/Images/nginx/init.sh
@@ -1,6 +1,14 @@
-#! /bin/sh
+#! /bin/bash
 ssh-keygen -A
 adduser -D username
 echo "username:password" | chpasswd
 /usr/sbin/sshd
-nginx -g "daemon off;"
+nginx
+
+while [[ $(pgrep nginx) ]] && [[ $(pgrep sshd) ]]; 
+do 
+	sleep 1 
+done
+
+pkill nginx
+pkill sshd
